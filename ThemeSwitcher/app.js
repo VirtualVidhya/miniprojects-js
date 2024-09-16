a4468af27944ed0a114d53fd26bef1d7461b6514
kharva <parvadesai404@gmail.com>
2024-09-16 17:34:16 +0530

    [Update] moon-icon in theme switcher project

--- ThemeSwitcher/app.js
+++ ThemeSwitcher/app.js
@@ -3,7 +3,7 @@
  * Look for a local storage value.
  * Fall back to system setting.
  * Fall back to light mode.
-*/
+ */
 function calculateSettingAsThemeString({
   localStorageTheme,
   systemSettingDark,
@@ -19,26 +19,24 @@ function calculateSettingAsThemeString({
   return "light";
 }
 
-
 // Utility function to update the button icon svg and title.
 function updateButton({ buttonEl, isDark }) {
   const newCta = isDark ? "Change to light theme" : "Change to dark theme";
-  
+
   buttonEl.setAttribute("title", newCta);
 
-  let sunIcon = document.querySelector('.sun-icon');
-  let moonIcon = document.querySelector('.moon-icon');
+  let sunIcon = document.querySelector(".sun-icon");
+  let moonIcon = document.querySelector(".moon-icon");
 
   console.log(sunIcon);
   console.log(moonIcon);
 
   if (isDark) {
-    sunIcon.classList.add('btn-icon-hidden');
-    moonIcon.classList.remove('btn-icon-hidden');
-  }
-  else {
-    sunIcon.classList.remove('btn-icon-hidden');
-    moonIcon.classList.add('btn-icon-hidden');
+    sunIcon.classList.add("btn-icon-hidden");
+    moonIcon.classList.remove("btn-icon-hidden");
+  } else {
+    sunIcon.classList.remove("btn-icon-hidden");
+    moonIcon.classList.add("btn-icon-hidden");
   }
 }
 
@@ -60,11 +58,9 @@ let currentThemeSetting = calculateSettingAsThemeString({
   systemSettingDark,
 });
 
-
 // 3. Update the theme setting and button text accoridng to current settings
 updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
 updateThemeOnHtmlEl({ theme: currentThemeSetting });
-
 
 // 4. Add an event listener to toggle the theme
 button.addEventListener("click", (event) => {
